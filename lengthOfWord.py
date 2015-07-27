0d3e8b9efa8ee50f118f50e682754eb903b0c0e4
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-27 20:19:31 +0300

    lenght last word

---
+++ lengthOfWord.py
@@ -0,0 +1,16 @@
+__author__ = 'cenk'
+
+
+def lengthLastWord(text):
+    if len(text) == 0:
+        return 0
+    word = ""
+    text = text.rstrip()
+    for i in range(len(text)):
+        if text[- (i + 1)].isalpha():
+            word += text[- (i + 1)]
+        if word != "" and not text[- (i + 1)].isalpha():
+            break
+    return len(word)
+
+print lengthLastWord("benim adim 2  sds cenkss 2  ")
