7bc08fcdf8b5d84bae15e8e8c1a4c6a9fcf0d7b1
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-30 22:23:27 +0300

    remove dups 2 added

--- RemoveDups.py
+++ RemoveDups.py
@@ -1,9 +1,10 @@
 __author__ = 'cenk'
 
 
-def removeDups(arr):
+def removeDups2(arr):
     if len(arr) == 0:
         return 0
+
     if len(arr) == 1:
         return 1
 
@@ -15,17 +16,27 @@ def removeDups(arr):
             if arr[j - 1] != arr[i]:
                 arr[j] = arr[i]
                 j += 1
+            elif arr[j - 1] == arr[i]:
+                arr[j] = arr[i]
+                j += 1
         elif arr[i] != arr[i + 1]:
+            arr[j] = arr[i]
+            j += 1
+        elif arr[i] == arr[i + 1] and arr[j -1] != arr[i]:
             arr[j] = arr[i]
             j += 1
         i += 1
 
     if j == 0:
-        j = 1
+        j = 2
+    if j == 1:
+        j = 2
+
+
     return j
 
-arr = []
-a = removeDups(arr)
+arr = [2,2,2,2]
+a = removeDups2(arr)
 print arr[:a]
 
 
