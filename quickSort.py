4f7a951db3dd975c1650fc4b52ca7fbc79b5cb16
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-08-02 18:17:19 +0300

    quick select

--- quickSort.py
+++ quickSort.py
@@ -2,20 +2,18 @@ __author__ = 'cenk'
 
 import random
 
-def quickSelect(arr):
-    
+def findKthLargest(arr,k):
+    if len(arr) == 1:
+        return arr[0]
+    kthSmallest = len(arr) - k + 1
+    return findKthSmallest(arr,kthSmallest)
 
-print quickSelect([42,7,3,1,10,13,11,22])
-
-def quickSort(arr):
-    if len(arr) == 0:
-        return []
-
+def findKthSmallest(arr,k):
     less =[]
     more = []
-    pivots = []
     pIndex = random.randint(0,len(arr) -1)
     p = arr[pIndex]
+    pivots = []
 
     for i in arr:
         if i == p:
@@ -25,6 +23,60 @@ def quickSort(arr):
         else:
             more.append(i)
 
-    return quickSort(less) + pivots + quickSort(more)
+    if k <= len(less):
+        return findKthSmallest(less,k)
+    elif k > len(less) + len(pivots):
+        return findKthSmallest(more,k - (len(less) + len(pivots)))
+    else:
+        return p
+
+print findKthLargest([3,2,1,5,6,4],2)
+
+
+# def quickSelect(arr,k):
+#     less =[]
+#     more = []
+#     pIndex = random.randint(0,len(arr) -1)
+#     p = arr[pIndex]
+#     pivots = []
+#
+#     for i in arr:
+#         if i == p:
+#             pivots.append(i)
+#         elif i < p :
+#             less.append(i)
+#         else:
+#             more.append(i)
+#
+#     if k <= len(less):
+#         return quickSelect(less,k)
+#     elif k > len(less) + len(pivots):
+#         return quickSelect(more,k - (len(less) + len(pivots)))
+#     else:
+#         return p
+#
+# print quickSelect([3,2,1,5,6,4],2)
+
+
+
+# def quickSort(arr):
+#     if len(arr) == 0:
+#         return []
+#
+#     less =[]
+#     more = []
+#     pivots = []
+#     pIndex = random.randint(0,len(arr) -1)
+#     p = arr[pIndex]
+#
+#     for i in arr:
+#         if i == p:
+#             pivots.append(i)
+#         elif i < p :
+#             less.append(i)
+#         else:
+#             more.append(i)
+#
+#     return quickSort(less) + pivots + quickSort(more)
 
 # print quickSort([4,3,1,10,11,12])
